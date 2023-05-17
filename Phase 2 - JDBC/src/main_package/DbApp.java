052f094d105bfeb8eae7b142065b88d389e7fa66
npapoutsakis <nikospapoutsak@gmail.com>
2023-05-17 03:30:52 +0300

    Analytical Assessment completed

--- Phase 2 - JDBC/src/main_package/DbApp.java
+++ Phase 2 - JDBC/src/main_package/DbApp.java
@@ -48,7 +48,7 @@ public class DbApp {
 		
 		db_terminate_connection();
 		
-		// Add a delay of 1.2 sec, just for fun :)
+		// Add a delay of 1s, just for fun :)
 		try {
 			TimeUnit.SECONDS.sleep(1);
 		} catch (InterruptedException e) {
@@ -106,7 +106,7 @@ public class DbApp {
 					
 					break;
 				case 4:
-					
+					getAnalyticalAssessment();
 					break;
 				case 5:
 					end_session();
@@ -119,9 +119,143 @@ public class DbApp {
 		return;
 	}
 	
-	private void changeStudentGrade() {
+	
+	// 1.4
+	private void getAnalyticalAssessment() {
 		
+		// Statement with parameters
+		PreparedStatement nameStatement, gradesStatement;
 		
+		try {
+			
+			String studentInfoQuery = "SELECT s.am, p.name, p.surname FROM \"Person\" p\r\n"
+									+ "INNER JOIN \"Student\" s ON s.amka = p.amka\r\n"
+									+ "WHERE am =  ?\r\n"
+									+ "";
+			
+			
+			String getGradesQuery = "SELECT DISTINCT  \r\n"
+									+ "	cr.semesterrunsin - (\r\n"
+									+ "			SELECT MIN(cr.semesterrunsin) \r\n"
+									+ "			FROM \"CourseRun\" cr \r\n"
+									+ "			INNER JOIN \"Register\" r ON (cr.course_code = r.course_code AND cr.serial_number = r.serial_number) \r\n"
+									+ "			WHERE r.amka = s.amka) + 1 AS \"SemesterNo\", \r\n"
+									+ "c.course_title, cr.course_code, r.final_grade, r.register_status\r\n"
+									+ "FROM \"Student\" s\r\n"
+									+ "INNER JOIN \"Register\" r ON r.amka = s.amka\r\n"
+									+ "INNER JOIN \"CourseRun\" cr ON (cr.course_code = r.course_code AND cr.serial_number = r.serial_number)\r\n"
+									+ "INNER JOIN \"Semester\" sem ON sem.semester_id = cr.semesterrunsin\r\n"
+									+ "INNER JOIN \"Course\" c ON c.course_code = cr.course_code\r\n"
+									+ "WHERE am = ? AND r.register_status IN ('pass', 'fail')\r\n"
+									+ "ORDER BY \"SemesterNo\"";
+			
+			//First get the student info
+			nameStatement = db_connection.prepareStatement(studentInfoQuery);
+			
+			// Get student am
+			String am = readString("Give student AM: ");
+			
+			nameStatement.setString(1, am);
+			
+			// Execute first query
+			ResultSet studentInfo = nameStatement.executeQuery();
+			
+			// Check if has info
+			if(!studentInfo.next()) {
+			   
+				//ResultSet is empty
+			    System.out.println("Cannot find AM in database!");
+			    
+			    //Release
+			    studentInfo.close();
+			    nameStatement.close();
+			    
+			    waitUser();
+			    
+			    return;
+			} 
+			else {
+			    
+				String name, surname;
+				
+				name = studentInfo.getString(2);
+				surname = studentInfo.getString(3);
+				
+				System.out.println("\n\t\t\t#Analytical Assessment#");
+				System.out.println("Student Info: ");
+				System.out.println("\tAM:      "+am);
+				System.out.println("\tName:    "+name);
+				System.out.println("\tSurname: "+surname+"\n");
+				
+				// made the first row, now access database to get grades aka new execution		    
+				// Do not need them any more
+				studentInfo.close();
+			    nameStatement.close();
+			    
+			    // Prepare grade query
+			    gradesStatement = db_connection.prepareStatement(getGradesQuery);
+			    gradesStatement.setString(1, am);
+			    
+			    ResultSet grades = gradesStatement.executeQuery();
+			    
+		        // Header
+		        System.out.printf("\t%-15s%-8s%-10s%-10s\n", "Course Code", "Grade", "Status", "Course Title");
+			    
+		        
+		        if(!grades.next()) {
+					
+		        	//ResultSet is empty
+				    System.out.println("Student has no grades at all!");
+				    
+				    //Release
+				    grades.close();
+				    gradesStatement.close();
+				    
+				    waitUser();
+				    
+				    return;	
+		        }
+		        else {
+		        	
+		        	int id = grades.getInt(1);
+		        	
+				    // Gather info
+		        	System.out.printf("\n\tSemester ID: %d\n", id);
+		        	System.out.printf("\t %-15s%-8s%-10s%-10s\n", grades.getString(3), grades.getFloat(4),grades.getString(5).toUpperCase(), grades.getString(2));
+				    
+		        	while(grades.next()) {	        
+				        // Change id when semester changes
+				    	if(grades.getInt(1) != id) {
+				    		id = grades.getInt(1);
+				    		System.out.printf("\n\tSemester ID: %d\n", id);
+				    	}	
+				    	// Print table rows
+				        System.out.printf("\t %-15s%-8s%-10s%-10s\n", grades.getString(3), grades.getFloat(4),grades.getString(5).toUpperCase(), grades.getString(2));
+				    }
+				    
+				    System.out.printf("\n");
+				    
+				    // Release
+				    grades.close();
+				    gradesStatement.close();
+		        	
+		        }   
+		        
+			}
+			
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		waitUser();
+		
+		return;
+	}
+	
+	
+	// 1.2
+	private void changeStudentGrade() {	
 		
 		
 		waitUser();
@@ -136,7 +270,8 @@ public class DbApp {
 
 		try {
 			pr_statement = 
-			this.db_connection.prepareStatement("SELECT s.am, r.course_code, r.final_grade, r.exam_grade, r.lab_grade, r.register_status FROM \"Register\" r\r\n"
+			this.db_connection.prepareStatement("SELECT s.am, r.course_code, r.final_grade, r.exam_grade, r.lab_grade, r.register_status "
+					+ "FROM \"Register\" r\r\n"
 					+ "INNER JOIN \"Student\" s USING (amka)\r\n"
 					+ "WHERE am = ?\r\n"
 					+ "AND course_code = ?\r\n"
@@ -168,6 +303,10 @@ public class DbApp {
 			else 
 				System.out.println("Cannot find such pair");
 		
+			pr_statement.close();
+			
+			output.close();
+			
 		}	
 		catch(Exception e){
 			e.printStackTrace();
@@ -179,12 +318,7 @@ public class DbApp {
 	}
 	
 	
-	/**
-	 * It reads an positive integer, zero included, from standard input
-	 * and returns it as value. In case of an error it returns -1
-	 * 
-	 * @param message The message that is appeared to the user asking for input
-	 */
+	// It reads an positive integer, zero included, from standard input and returns it as value. In case of an error it returns -1
 	private int readPositiveInt(String message) {	
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String str;
@@ -210,12 +344,7 @@ public class DbApp {
 	}
 	
 	
-	/**
-	 * It reads a string from standard input and returns it as value. 
-	 * In case of an error it returns null
-	 * 
-	 * @param message The message that is appeared to the user asking for input
-	 */
+	//It reads a string from standard input and returns it as value. In case of an error it returns null
 	private String readString(String message) {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print(message);
@@ -233,6 +362,7 @@ public class DbApp {
 		return;
 	}
 
+	
 	// Printing choice menu
 	private void printMenu() {
 		System.out.println("\n\n-------------JAVA JDBC API----------------");		
@@ -240,7 +370,7 @@ public class DbApp {
 		System.out.println("1. Get course grade of student............");
 		System.out.println("2. Change student grade...................");
 		System.out.println("3. TODO");
-		System.out.println("4. TODO");
+		System.out.println("4. Analytical assessment of student.......");
 		System.out.println("5. Exit Application.......................");
 		System.out.println("------------------------------------------");
 		System.out.print("Choice: ");
--- Phase 2 - JDBC/src/main_package/Launcher.java
+++ Phase 2 - JDBC/src/main_package/Launcher.java
@@ -2,7 +2,7 @@ package main_package;
 
 public class Launcher {
 
-	public static void main(String[] args) throws InterruptedException {	
+	public static void main(String[] args) {	
 		DbApp app = new DbApp();
 	
 		// Start connection to database using driver and prints choice menu
